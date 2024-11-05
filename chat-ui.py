edff2bf4bab16025d22f3a330911d5c3a9abd49f
Rick Umali <rumali@veracode.com>
2024-11-05 11:18:43 -0500

    Fixes for Python 3.8
    I needed to make these changes in order to run this little app
    on my WSL 2 (on Windows). The stock Python on WSL 2 is Python 3.8,
    which required a lower version of NumPy and a change in type
    hinting.
    
    See:
    
    https://stackoverflow.com/questions/63460126/typeerror-type-object-is-not-subscriptable-in-a-function-signature

--- chat-ui.py
+++ chat-ui.py
@@ -1,13 +1,15 @@
+from typing import List
 import chainlit as cl
 from ctransformers import AutoModelForCausalLM
 
 
-def get_prompt(instruction: str, history: list[str] = None) -> str:
+def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the questions in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
     if len(history) > 0:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
+    print(prompt)
     return prompt
 
 
